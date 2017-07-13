6313ee25d314ac272b5442dbca3a90a62d3a9ca0
talJoffe <joffe.tal@gmail.com>
2017-07-13 15:38:39 +0300

    wip - incorporating redux (based on redux todo app) - almost there

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import './style/App.css';
 import Header from "./components/Header"
 import SideBar from "./components/SideBar";
+import PostContent from "./components/PostContent";
+import Footer from "./components/Footer"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Header></Header>
+          <PostContent/>
           <SideBar/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Footer/>
       </div>
     );
   }
 }
+
+const initialState = {
+    postLinks: [{id:123, text:'hello', show: true},{id:124, text:'hi', show: false},{id:125, text:'previet', show: false}]
+};
 
 export default App;
