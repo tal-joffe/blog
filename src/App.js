41cf862fed91673c5d7d8dbc18519bd6f6d32d23
talJoffe <joffe.tal@gmail.com>
2017-07-16 12:04:34 +0300

    wip - doing refactor.. trouble with Sidebar component (VisiblePostList)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
 import './style/App.css';
+import React from 'react';
 import Header from "./components/Header"
 import SideBar from "./components/SideBar";
-import PostContent from "./components/PostContent";
 import Footer from "./components/Footer"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header></Header>
-          <PostContent/>
-          <SideBar/>
-          <Footer/>
-      </div>
-    );
-  }
-}
 
-const initialState = {
-    postLinks: [{id:123, text:'hello', show: true},{id:124, text:'hi', show: false},{id:125, text:'previet', show: false}]
-};
+import ContentContainer from './containers/ContentContainer'
+import VisiblePostsList from './containers/VisiblePostsList'
+
+const App = () => (
+    <div>
+        <Header/>
+        <ContentContainer/>
+        <VisiblePostsList/>
+        <Footer/>
+    </div>
+)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,203 +3,22 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { Provider, connect } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
+import ContentContainer from './containers/ContentContainer'
+import VisiblePostsList from './containers/VisiblePostsList'
+import blogApp from './reducers'
+import App from './App'
 
 
-/////////////////////////////////////////////////////////////////////////////////
-////////////////////////        reducers     /////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-const todo = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return {
-                id: action.id,
-                text: action.text,
-                completed: false
-            };
-        case 'TOGGLE_TODO':
-            if (state.id !== action.id) {
-                return state;
-            }
 
-            return {
-                ...state,
-                completed: !state.completed
-            };
-        case 'SHOW_THIS_POST_IN_CONTENT':
-            if (state.id === action.id) {
-                return {
-                    ...state,
-                    completed: true
-                };
-            }
-            else {
-                return {
-                    ...state,
-                    completed: false
-                };
-            }
-        default:
-            return state;
-    }
+//todo: consider https://wix.github.io/react-templates/
+
+const initialState = {//todo: get this from config or something..
+    postLinks: [{id:123, text:'hello', completed: false},{id:124, text:'hi', completed: false},{id:125, text:'previet', completed: false}]
 };
-
-const todos = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                todo(undefined, action)
-            ];
-        case 'TOGGLE_TODO':
-            return state.map(t =>
-                todo(t, action)
-            );
-        case 'SHOW_THIS_POST_IN_CONTENT':
-            return state.map(t =>
-                todo(t, action)
-            );
-        default:
-            return state;
-    }
-};
-
-
-const todoApp = combineReducers({
-    todos
-});
-/////////////////////////////////////////////////////////////////////////////////
-////////////////////////        actions     /////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-const toggleTodo = (id) => {
-    return {
-        type: 'TOGGLE_TODO',
-        id
-    };
-};
-
-const showPostInContent = (id) => {
-    return {
-        type: 'SHOW_THIS_POST_IN_CONTENT',
-        id
-    };
-};
-
-
-/////////////////////////////////////////////////////////////////////////////////
-////////////////////////    components      /////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-///////////////////   content   ////////////////////////////////
-
-const Content = ({text}) => {
-    return (
-        <div>
-            <h2>this is content:</h2>
-            <p>
-                {text}
-            </p>
-        </div>
-    );
-};
-
-///////////////////   toodo   ////////////////////////////////
-
-const Todo = ({
-                  onClick,
-                  completed,
-                  text
-              }) => (
-    <li
-        onClick={onClick}
-        style={{
-            textDecoration:
-                completed ?
-                    'font-weight' : 'bold'
-        }}
-    >
-        {text}
-    </li>
-);
-
-const TodoList = ({
-                      todos,
-                      onTodoClick
-                  }) => (
-    <ul>
-        {todos.map(todo =>
-            <Todo
-                key={todo.id}
-                {...todo}
-                onClick={() => onTodoClick(todo.id)}
-            />
-        )}
-    </ul>
-);
-
-
-/////////////////////////////////////////////////////////////////////////////////
-////////////////////////    containers      /////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-
-const mapStateToContentProps = (state, ownProps) => {
-    return {
-        text:
-         state.todos.filter(t=>t.completed).map(t=>t.text)
-    };
-};
-const ContentContainer = connect(mapStateToContentProps)(Content);
-
-
-
-
-const mapStateToTodoListProps = (
-    state
-) => {
-    return {
-        todos: state.todos
-    };
-};
-const mapDispatchToTodoListProps = (
-    dispatch
-) => {
-    return {
-        onTodoClick: (id) => {
-            dispatch(showPostInContent(id));
-        }
-    };
-};
-const VisibleTodoList = connect(
-    mapStateToTodoListProps,
-    mapDispatchToTodoListProps
-)(TodoList);
-
-
-
-/////////////////////////////////////////////////////////////////////////////////
-////////////////////////    app             /////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-
-const TodoApp = () => (
-    <div>
-        <VisibleTodoList />
-        <ContentContainer/>
-    </div>
-);
-const initialState = {
-    todos: [{id:123, text:'hello', completed: false},{id:124, text:'hi', completed: false},{id:125, text:'previet', completed: false}]
-};
-
-
-/////////////////////////////////////////////////////////////////////////////////
-////////////////////////    index           /////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 
 ReactDOM.render(
-    <Provider store={createStore(todoApp,initialState)}>
-        <TodoApp />
+    <Provider store={createStore(blogApp,initialState)}>
+        <App />
     </Provider>,
     document.getElementById('root')
-);
+)
