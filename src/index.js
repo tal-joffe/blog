3ae49b1563692e457caecc8d64877baad4d118be
talJoffe <joffe.tal@gmail.com>
2017-07-23 18:09:06 +0300

    moved api call to redux with redux-thunk

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App'
 import store, { history } from './store'
 
 
-//todo: use code from here: https://github.com/misterfresh/react-drive-cms to write methods to get data from google Drive and render as post content
+//todo: use code from here: https://github.com/misterfresh/react-drive-cms to write methods to get data from google Drive and render as post driveID
 
 
 ReactDOM.render(
