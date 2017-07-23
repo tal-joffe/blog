7488195ccd77b55efa2e0303a1749be424fe527b
talJoffe <joffe.tal@gmail.com>
2017-07-23 13:16:13 +0300

    wip - getting single post from drive! - need refactoring

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import { ConnectedRouter } from 'react-router-redux'
 import App from './App'
 import store, { history } from './store'
 
+
+//todo: use code from here: https://github.com/misterfresh/react-drive-cms to write methods to get data from google Drive and render as post content
+
+
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
