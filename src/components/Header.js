47841c7b98ca39c77a75006043c897cd98884abd
talJoffe <joffe.tal@gmail.com>
2017-07-24 11:46:48 +0300

    bootstrap navbar is working fine.

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,28 +10,28 @@ import { LinkContainer } from 'react-router-bootstrap'
 //todo: think about navbar... might want to use something else
 
 const Header = () => (
-    <div className="App-header">
-        <h2>Title</h2>
-        <Link to="/blog">Blog</Link>
-        <Link to="/about">About</Link>
+    <div>
+        <div className="App-header">
+            <h2>Title</h2>
+        </div>
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <p>Tal's site</p>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+                <LinkContainer to="/blog">
+                <NavItem eventKey={1} href="#">Blog</NavItem>
+                </LinkContainer>
+                <NavItem eventKey={2} href="#">CV</NavItem>
+                <LinkContainer to="/about">
+                <NavItem eventKey={2} href="#">About</NavItem>
+                </LinkContainer>
+            </Nav>
+        </Navbar>
+
     </div>
 )
-
-
-// <Navbar>
-// <Navbar.Header>
-// <Navbar.Brand>
-// <h2>Tal's site</h2>
-// </Navbar.Brand>
-// </Navbar.Header>
-// <Nav>
-// <LinkContainer to="/blog">
-//     <NavItem eventKey={1}>Blog</NavItem>
-//     </LinkContainer>
-//     <LinkContainer to="/about">
-//     <NavItem eventKey={2}>About</NavItem>
-//     </LinkContainer>
-//     </Nav>
-//     </Navbar>
 
 export default Header;
